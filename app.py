e61496c3538c881d084db0ec43784a6760e83f75
Jorge Muñoz <jlmunozfdev@outlook.com>
2023-07-21 16:28:08 -0500

    Modify principal

--- app.py
+++ app.py
@@ -3,14 +3,19 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-@app.route("/home")
+@app.route("/login")
 def index():
-    return render_template('index.html')
+    return render_template('login.html')
 
 
 @app.route("/principal")
 def principal():
     return render_template('principal.html')
+
+@app.route("/component")
+def component():
+    return render_template('component.html')
+
 
 
 if __name__ == '__main__':
